1478257e84489ff6cfafc241676f38f3eb4ae25a
MaherHazbaz <maherhazbaz7@gmail.com>
2024-08-30 11:53:48 +0530

    vcv

--- server/src/Pages/Todo.jsx
+++ server/src/Pages/Todo.jsx
@@ -11,7 +11,7 @@ const Index = () => {
   const [editText, setEditText] = useState(""); // Track the new text for the item being edited
   const [todos, setTodos] = useState([]);
 
-  const base_url = "http://localhost:5176";
+  const base_url = "http://localhost:5173";
 
   const handleTodo = async () => {
     try {
